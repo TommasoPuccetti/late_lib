6fcddd324661edf7fd42ad169185a757f01397ec
TommasoPuccetti <tommaso.puccetti@unifi.it>
2025-03-24 10:25:01 +0100

    Evaluator functions added. TODO final resutls function

---
+++ evaluator.py
@@ -0,0 +1,259 @@
+import os
+import numpy as np
+import sklearn
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import pandas as pd
+from config import *
+from loader import PathManager
+
+
+class Evaluator:
+    """Evaluate performance of a detector given prediction probabilities and labels.
+     Evaluator can perform:
+     - overall evaluation: it returns the evaluation calculated by the  
+     Attributes:
+         results_p: data output path to store the results of the evaluation.
+    """
+    def __init__(self, paths: PathManager):
+        self.overall = {}
+        self.results_p = paths.results_p
+    
+    def eval_overall(self, test_y, preds):
+        acc = sklearn.metrics.accuracy_score(test_y, preds)
+        rec = sklearn.metrics.recall_score(test_y, preds)
+        prec = sklearn.metrics.precision_score(test_y, preds)
+        f1 = sklearn.metrics.f1_score(test_y, preds)
+        tn, fp, fn, tp = sklearn.metrics.confusion_matrix(test_y, preds).ravel()
+        overall = {
+            "accuracy": acc,
+            "recall": rec,
+            "precision": prec,
+            "f1-score": f1,
+            "tn": tn,
+            "fp":fp,
+            "fn": fn,
+            "tp": tp
+        }
+        self.overall = overall 
+        print("Accuracy: {}, \nRecall: {}, \nPrecision: {}, \nF1-Score: {}".format(acc, rec, prec, f1))
+        print("TP: {}, \nFP: {}, \nTN: {}, \nFN: {}".format(tp, fp, tn, fn))
+        print(PRINT_SEPARATOR)
+        return overall
+    
+    def plot_roc(self, test_y, preds_proba, results_p=None):
+        if results_p == None:
+            results_p = self.results_p
+        plt.figure(dpi=400)
+        fpr, tpr, _ = metrics.roc_curve(test_y,  preds_proba[:,1])
+        plt.plot(fpr, tpr)
+        plt.ylabel('Recall')
+        plt.xlabel('False Positive Rate')
+        plt.savefig(os.path.join(results_p, "roc_curve,pdf"), format='pdf', bbox_inches='tight')
+        plt.show()
+
+    def plot_precision_recall(self, test_y, preds_proba, results_p=None):
+        if results_p == None:
+            results_p = self.results_p
+        plt.figure(dpi=400)
+        fpr, tpr, _ = metrics.precision_recall_curve(test_y,  preds_proba[:,1])
+        plt.plot(fpr, tpr)
+        plt.ylabel('Precision')
+        plt.xlabel('Recall')
+        plt.savefig(os.path.join(results_p, "precision_recall_curve,pdf"), format='pdf', bbox_inches='tight')
+        plt.show()
+
+    def plot_curves(self, test_y, preds_proba, results_p=None):
+        if results_p == None:
+            results_p = self.results_p
+        self.plot_precision_recall(test_y, preds_proba, results_p)
+        self.plot_roc(test_y, preds_proba, results_p)
+                    
+    def eval_fpr_latency(self, test_y, test_multi, test_timestamp, test_seq, preds_proba, desired_fpr=DESIRED_FPR, results_p=None, verbose=False):
+        
+        #if the path is not provided by argument take the one in object param.
+        if results_p == None:
+            results_p = self.results_p
+        
+        #compute the roc curve using the model prediction probabilities     
+        fpr, tpr, thresholds = metrics.roc_curve(test_y, preds_proba[:,1])
+        fpr_indexes = []
+        #select the index of the fpr to consider 
+        for val in desired_fpr:
+            index = np.argmax(fpr > val)
+            fpr_indexes.append(index)
+        #find the threshold to obtain such fpr
+        fpr_thresholds = thresholds[fpr_indexes] 
+        bin_preds_fpr = []
+        for val in fpr_thresholds:
+            bin_pred = (preds_proba > val).astype(int)[:, 1]
+            bin_preds_fpr.append(bin_pred)
+            if verbose: print(bin_preds_fpr)
+
+        sequences_results = []
+        #store fpr values with relative binary prediction obtaine from using a specific threshold
+        for val, des_fpr in zip(bin_preds_fpr, DESIRED_FPR):
+            bin_pred = val
+        #REFACTOR TILL HERE IN DEDICATED FUNCTION FOR FPR DEFINITION 
+
+            fpr_results = pd.DataFrame(columns=['start_idx_attack', 'end_idx_attack', 'attack_duration', 'time_to_detect', 'idx_detection_abs', 'idx_detection_rel', 'attack_len', 'attack_type','fpr','tpr','pr','rec','tn', 'fp','fn','tp','target_fpr', 'detected'])
+            last = 0
+            i = 0
+    
+            for seq in test_seq:
+                seq_y = test_y[seq]
+                seq_preds = bin_pred[last: last + seq_y.shape[0]]
+                seq_preds = np.array(seq_preds)
+    
+                seq_check = test_y[last: last + seq_y.shape[0]]
+                y_test_atk= np.where(test_y[seq] == 1)[0]
+                y_test_norm= np.where(test_y[seq] ==  0)[0]
+    
+                conf_matrix = sklearn.metrics.confusion_matrix(seq_y, seq_preds)
+                #TODO CASES WHERE SOME METRICS IN CONF MATRIX ARE NOT AVAILABLE TRY CATCH
+                tn, fp, fn, tp = conf_matrix.ravel()
+                pr = sklearn.metrics.precision_score(seq_y, seq_preds)
+                rec = sklearn.metrics.recall_score(seq_y, seq_preds)
+        
+                preds_1 = seq_preds[y_test_atk]
+        
+                date_time_0 = test_timestamp[seq[y_test_atk[0]]]
+                date_time_last = test_timestamp[seq[y_test_atk[len(y_test_atk)-1]]]
+                attack_time = date_time_last - date_time_0
+        
+                if 1 in seq_preds[y_test_atk]:
+                    index_rel = np.where(seq_preds[y_test_atk] == 1)[0][0]
+                    index = seq[y_test_atk[index_rel]]
+        
+                    date_time_index = test_timestamp[index]
+                    detection_time = date_time_index - date_time_0
+                    detected = 1
+                else:
+                    index_rel = len(seq) - 1
+                    index = seq[y_test_atk[index_rel]]
+                    #TODO ADD A TIME DELTA TO LATENCY WHEN SEQUENCE IS NOT DETECTED
+                    detection_time = attack_time
+                    detected = 0
+            
+                fpr_results.loc[i] = pd.Series({'start_idx_attack': seq[y_test_atk[0]], 'end_idx_attack': seq[y_test_atk[len(y_test_atk)-1]],
+                                   'attack_duration': attack_time , 'time_to_detect': detection_time, 'idx_detection_abs': index,
+                                   'idx_detection_rel':index_rel, 'attack_len': y_test_atk.shape[0],
+                                   'attack_type':test_multi[seq[y_test_atk[0]]], 'tpr': tpr, 'pr': pr, 'rec': rec,
+                                   'fp':fp, 'fn':fn, 'tp':tp, 'tn':tn, 'target_fpr':des_fpr, 'detected': detected})
+                last = last+len(seq_y)
+                i = i+1
+                
+                #LOG ALL INTERMEDIATE IF VERBOSE 
+                #fpr_results.to_csv(os.path.join(results_p,  str(des_fpr) + '.csv'), index=None)
+            
+            sequences_results.append(fpr_results)        
+        return sequences_results
+
+    def avg_fpr_latency(self, sequences_results, results_p=None):
+        #if the path is not provided by argument take the one in object param.
+        if results_p == None:
+            results_p = self.results_p
+            
+        for fpr_result in sequences_results: 
+            print("---------------------------------")
+            print(len(sequences_results))
+            print("---------------------------------")
+            print(fpr_result)
+            df = fpr_result
+            # calculate time_to_detect (attack latency) for all the detected sequences 
+            df_detect = df[df['detected'] != 0]
+            df_detect['time_to_detect'] = pd.to_timedelta(df['time_to_detect']).dt.total_seconds()
+            
+            #calculate sequence detection rate
+            grouped_df = df_detect.groupby('attack_type')
+            print(grouped_df)
+            
+            grouped_df_det = df_detect.groupby('attack_type').size().reset_index(name='count_det')
+            grouped_df_tot = df.groupby('attack_type').size().reset_index(name='count_tot')
+            print(grouped_df_tot)
+
+            detection_rate_df = pd.merge(grouped_df_det, grouped_df_tot, on='attack_type', how='outer')
+            detection_rate_df['count_ratio'] = detection_rate_df['count_det'] / detection_rate_df['count_tot']
+            target_fpr = str(df['target_fpr'].unique()[0])
+            detection_rate_df['target_fpr'] = target_fpr
+            #print(detection_rate_df)
+        
+            # Calculate average, min, max, and std for each group
+            avg_result_df = grouped_df.agg({
+                'attack_len': ['mean', 'min', 'max', 'std'],
+                'fpr': 'mean',
+                'pr': 'mean',
+                'rec': 'mean',
+                'time_to_detect': ['mean', 'min', 'max', 'std'],
+                'idx_detection_rel': ['mean', 'min', 'max', 'std']
+            }).reset_index()
+
+    
+            
+            # Calculate aggregated statistics
+            result_dict = {
+                'detected_sequences': len(df_detect),
+                'percent_detected_sequences':(df.shape[0] - len(df_detect))/len(df_detect),
+                'avg_time_to_detect': df_detect['time_to_detect'].mean(),
+                'std_time_to_detect': df_detect['time_to_detect'].std(),
+                'min_time_to_detect': df_detect['time_to_detect'].min(),
+                'max_time_to_detect': df_detect['time_to_detect'].max(),
+                'avg_idx_detection_rel': df_detect['idx_detection_rel'].mean(),
+                'std_idx_detection_rel': df_detect['idx_detection_rel'].std(),
+                'min_idx_detection_rel': df_detect['idx_detection_rel'].min(),
+                'max_idx_detection_rel': df_detect['idx_detection_rel'].max(),
+                'target_fpr':df['target_fpr']}
+            
+            all_result_df = pd.DataFrame([result_dict])
+        
+            avg_result_df.columns = ['_'.join(col) for col in avg_result_df.columns]
+            
+            # Save the result and another DataFrame in the same Excel file
+            with pd.ExcelWriter(os.path.join(results_p,  target_fpr + '_all.xlsx'), engine='xlsxwriter') as writer:
+                df.to_excel(writer, index=False, sheet_name='sequences_results')
+                df_detect.to_excel(writer, index=False, sheet_name='detected_sequences_results')
+                avg_result_df.to_excel(writer, index=False, sheet_name='avg_results_for_attacks_type')
+                detection_rate_df.to_excel(writer, index=False, sheet_name='attacks_detection_rate')
+                all_result_df.to_excel(writer, index=False, sheet_name='all_summary')
+
+    def fpr_latency(self):
+        i = 0
+        df = pd.read_excel(root+split+xlsx_files[0], sheet_name='avg_results_for_attacks_type')
+        features = df['attack_type_'].unique()
+        features = list(features)
+        features.append('target_fpr')
+        print(features)
+        
+        df_out = pd.DataFrame(columns=features)
+        for file in xlsx_files:
+            print('---------------------------')
+            df = pd.read_excel(root + split + file, sheet_name='avg_results_for_attacks_type')
+            features = df['attack_type_'].unique()
+            features = list(features)
+            features.append('target_fpr')
+            print(features)
+        
+            latency = []
+            for index, row in df.iterrows():
+                latency.append(row['time_to_detect_mean'])
+                print(row['time_to_detect_mean'])
+                print(row['attack_type_'])
+                print('----')
+        
+            df_1 = pd.read_excel(root + split + file, sheet_name='sequences_results')
+        
+            # Round the numbers for precision, change 3.49275472 to a rounded value
+            my_dict = dict(zip(features, latency))
+            my_dict.update({'target_fpr': round(df_1['target_fpr'].loc[1], 5)})  # Adjust precision
+            print(my_dict)
+        
+            # Convert the dictionary to a pandas Series for adding to the dataframe
+            series_to_add = pd.Series(my_dict, index=features)
+            df_out = df_out.append(series_to_add, ignore_index=True)
+        
+        # Save the dataframe to a CSV file
+        path = root + split + 'all_results.csv'
+        df_out.to_csv(path, index=None)
+
+
---
+++ loader.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd 
+import os
+from config import *
+
+
+#TODO a function that takes your data_frame and produces dataset files needed for the evaluation method.
+
+class PathManager:
+    """Initialize the paths to files needed for evaluation.
+     Attributes:
+         root: data root folder.
+         dataset: name of the dataset.
+         models: name of the model.
+         dataset_root: points to dataset folder e.g. (./data/dos_mqtt_iot).
+         files: points to the "dataset" folder of dataset_root (contains the dataset and label folder)
+         models: points to the folder where folders dedicated to models are stored.
+         target_model: points to a specific dataset folder (contains model and prediction folder)
+         test_y_p: points to test binary label.
+         test_multi_p: points to test multi-class label.
+         timestamp_test_p: points to test timestamp.
+         test_seq_p: points to test sequences indices.
+         preds_proba_p: points to model prediction probabilities.
+         preds_p: points to model binary predictions.
+    """
+    def __init__(self, dataset, model, root=ROOT, verbose=False):
+        self.root = root
+        self.dataset = dataset
+        self.model = model
+        self.dataset_root = os.path.join(self.root, self.dataset)
+        self.files = os.path.join(self.dataset_root, "dataset")
+        self.models = os.path.join(self.dataset_root, "models")
+        self.target_model = os.path.join(self.models, self.model)
+        if verbose:
+            print_paths()
+    
+    @property
+    def test_y_p(self):
+        return os.path.join(self.files, "labels/test_y.npy")
+    @property
+    def test_multi_p(self):
+        return os.path.join(self.files, "labels/test_multi_label.npy")
+    @property
+    def test_timestamp_p(self):
+        return os.path.join(self.files, "labels/test_timestamp.npy")
+    @property
+    def test_seq_p(self):
+        return os.path.join(self.files, "labels/test_sequences.pkl")
+    @property
+    def preds_proba_p(self):
+        return os.path.join(self.target_model, "preds/preds_proba.npy")
+    @property
+    def preds_p(self):
+        return os.path.join(self.target_model, "preds/preds.npy")
+    @property
+    def results_p(self):
+        return os.path.join(self.target_model, "results")
+    
+    def print_paths():
+        print("Dataset:  {}, \nModel:  {}".format(preds, self.model))
+        print("Dataset path:  {}, \nModel path:  {}".format(self.files, self.target_model))
+        print("test_y path:  {}, \ntest_seq path:  {}, \ntest_multi path {}".format(test_y_p, test_seq_p, test_multi_p))
+        print("preds_proba path:  {}, \npreds path:  {}".format(preds_proba_p, preds))
+        print(PRINT_SEPARATOR)
+
+
+class DataLoader:
+    """Load files needed for evaluation.
+     Attributes: 
+         path_manager: an instance of the PathManager class that points to data to load.
+         test_y: .npy binary label.
+         test_multi: .npy multi-class label.
+         timestamp_test: .npy test timestamp.
+         test_seq: .npy test sequences indices.
+         preds_proba: .npy prediction probabilities of the model.
+         preds: .npy binary predictions of the model.
+    """
+    def __init__(self, paths: PathManager):
+        self.paths = paths
+    
+    @property
+    def test_y(self):
+        return np.load(self.paths.test_y_p, allow_pickle=True)
+    @property
+    def test_multi(self):
+        return np.load(self.paths.test_multi_p, allow_pickle=True)
+    @property
+    def test_timestamp(self):
+        return np.load(self.paths.test_timestamp_p, allow_pickle=True)
+    @property
+    def test_seq(self):
+        return np.load(self.paths.test_seq_p, allow_pickle=True)
+    @property
+    def preds_proba(self):
+        return np.load(self.paths.preds_proba_p, allow_pickle=True)
+    @property
+    def preds(self):
+        return np.load(self.paths.preds_p, allow_pickle=True)
